85d7776156684fd0da33408066d3236258310555
PedroPertusi <pedrovazpertusi@gmail.com>
2023-10-23 15:42:23 -0300

    controller update, request param

--- aluguel/src/main/java/com/palmeiras/aluguel/aluguel/AluguelController.java
+++ aluguel/src/main/java/com/palmeiras/aluguel/aluguel/AluguelController.java
@@ -3,19 +3,24 @@ package com.palmeiras.aluguel.aluguel;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.palmeiras.aluguel.aluguel.Enum.Status;
 import com.palmeiras.aluguel.aluguel.dto.AluguelReturnDTO;
 
-@Controller
+@RestController
+@RequestMapping("/aluguel")
 public class AluguelController {
 
     @Autowired
     private AluguelService aluguelService;
 
-    public List<AluguelReturnDTO> findAlugueis(Status status, String cpfCorretor, String cpfLocatario) {
 
+    @GetMapping
+    public List<AluguelReturnDTO> findAlugueis(@RequestParam Status status, @RequestParam String cpfCorretor, @RequestParam String cpfLocatario) {
         return aluguelService.findAlugueis(status, cpfCorretor, cpfLocatario);
 
     }
